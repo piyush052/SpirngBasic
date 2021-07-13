3e5d6b0a978cdf0603b9ca9b381f648a78a34d20
Piyush <piyushupadhyay052@gmail.com>
2021-07-13 09:21:20 +0530

    Initial commit

---
+++ src/main/java/com/piyush/SpirngBasic/SpirngBasicApplication.java
@@ -0,0 +1,13 @@
+package com.piyush.SpirngBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piyush/SpirngBasic/SpirngBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyush.SpirngBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
