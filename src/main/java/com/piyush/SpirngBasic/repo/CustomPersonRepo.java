45e2cd823f3d39717ad84136c3a2628cc61dd3c8
Piyush <piyushupadhyay052@gmail.com>
2021-08-06 08:03:19 +0530

    Added Bean Post Processor anf bean FactoryPostProcessor

--- src/main/java/com/piyush/SpirngBasic/repo/CustomPersonRepo.java
+++ src/main/java/com/piyush/SpirngBasic/repo/CustomPersonRepo.java
@@ -2,6 +2,7 @@ package com.piyush.SpirngBasic.repo;
 
 import com.piyush.SpirngBasic.entities.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -13,7 +14,7 @@ public class CustomPersonRepo implements CustomRepo{
     @Autowired
     EntityManager entityManager ;
     @Override
-    public List<Person> findAll() {
+    public List<Person> findAll(Sort var1) {
 
         entityManager.getCriteriaBuilder().and();
         return null;
--- src/main/java/com/piyush/SpirngBasic/repo/CustomRepo.java
+++ src/main/java/com/piyush/SpirngBasic/repo/CustomRepo.java
@@ -1,9 +1,10 @@
 package com.piyush.SpirngBasic.repo;
 
 import com.piyush.SpirngBasic.entities.Person;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
 public interface CustomRepo {
-    List<Person> findAll();
+    List<Person> findAll(Sort var1);
 }
